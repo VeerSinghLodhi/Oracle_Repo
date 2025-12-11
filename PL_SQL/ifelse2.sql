d44d82679c26c86cdea272e2291174d1513e6236
Veer Singh Lodhi <veerlodhi54@gmail.com>
2025-12-11 22:28:52 +0530

    Add files via upload

---
+++ PL_SQL/ifelse2.sql
@@ -0,0 +1,15 @@
+declare 
+	num number;
+begin
+	num:=&num;
+	if num>0
+	then
+		dbms_output.put_line('Number is Positive');
+	elsif num<0
+	then
+		dbms_output.put_line('Number is Negative');
+	else
+		dbms_output.put_line('Number is Zero');
+	end if;
+end;
+/
